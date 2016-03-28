e05a1d1dc731ddbcaedf259d88f06d60619e1312
Heeyoung Jung <appcreator05@AppCreator05ui-iMac.local>
2016-03-28 20:53:41 +0900

    #5 implement TextView Result Activity

--- app/src/main/java/kr/hs/emirim/young24/monpizza/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/young24/monpizza/ResultActivity.java
@@ -2,15 +2,21 @@ package kr.hs.emirim.young24.monpizza;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by appcreator05 on 2016. 3. 28..
  */
 public class ResultActivity  extends AppCompatActivity {
 
+    TextView mReult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mReult = (TextView)findViewById(R.id.result);
+        mReult.setText("결과");
     }
 }
