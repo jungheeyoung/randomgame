fb72c12110c276d898135d48b5a8dbb24d963a8f
Heeyoung Jung <appcreator05@AppCreator05ui-iMac.local>
2016-03-28 20:42:02 +0900

    #3 implement input username

--- app/src/main/java/kr/hs/emirim/young24/monpizza/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/young24/monpizza/MainActivity.java
@@ -2,12 +2,25 @@ package kr.hs.emirim.young24.monpizza;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mName;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//객체 이어주는 것, 한번만 처리하는것
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mName = (EditText)findViewById(R.id.name);//id값을 가지고 객체를 찾아서 가져와줌,activity_main에서 찾아서 가져와줌/ mName이 EditText이므로 그에 맞게 타입캐스팅해준다. xml에 있는 EditText를 맘대로 가져올 수 o
+
+    }
+
+    @Override
+    protected void onResume() { //매번 처리해야 되는 것
+        super.onResume();
+        mName.setText("");//text공간을 비우게 해줌
     }
 }
