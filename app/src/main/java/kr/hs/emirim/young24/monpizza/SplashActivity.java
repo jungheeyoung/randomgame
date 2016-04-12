034b2eb890a8648ae5b0788a27c0c0075f25f8b8
Heeyoung Jung <appcreator05@AppCreator05ui-iMac.local>
2016-04-12 19:19:17 +0900

    #9 Activity Animation

--- app/src/main/java/kr/hs/emirim/young24/monpizza/SplashActivity.java
+++ app/src/main/java/kr/hs/emirim/young24/monpizza/SplashActivity.java
@@ -12,12 +12,13 @@ public class SplashActivity extends ActionBarActivity{
 
         setContentView(R.layout.activity_splash);
 
-        Handler handler = new Handler();
+        Handler handler = new Handler();//일정시간 지난 후 없어지는 것도 가능하지만 타이머도 만들 수 있다.
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(getApplicationContext(),MainActivity.class);//activity 시작할 때 필요한 객체 / 현재 사용하는 문맥객체를 받을 수 있음
-                startActivity(intent); // 시작!
+                startActivity(intent); // 시작! 자동 머신에서 자동으로 intent 생성
+                overridePendingTransition(0,R.anim.zoom_exit);   //무시하다
                 finish(); //현재 splash Acitvity를 마감해줌,끝
             }
         },3000); //~한 뒤에 ,3초 delay한 다음에 run method작동
